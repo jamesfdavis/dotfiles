b5ef481f53f4f49a71a27fef5933674fe11bfd1e
James Davis <ragingsmurf@gmail.com>
2019-04-27 15:45:52 -0400

    Fresh smartcard

--- yk-setup.sh
+++ yk-setup.sh
@@ -1,9 +1,13 @@
-# ~/.gnupg profiles get mapped to yk hardware devices.
-
 # nuke existing profile
-
 rm -rf ~/.gnupg 
+
+mkdir ~/.gnupg
 
 # Getting settings
 curl -o ~/.gnupg/gpg.conf https://raw.githubusercontent.com/drduh/config/master/gpg.conf
 
+# Import Key
+gpg --import $PUBLIC_KEY
+
+# Trust new key
+gpg --edit-key $KEYID
