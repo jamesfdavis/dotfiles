aa3b30312a80f532e08ed0b8e07818667a896f77
James Davis <ragingsmurf@gmail.com>
2019-04-27 15:51:49 -0400

    permissions

--- yk-setup.sh
+++ yk-setup.sh
@@ -1,10 +1,11 @@
 # nuke existing profile
 rm -rf ~/.gnupg 
 
-mkdir ~/.gnupg
-
 # Getting settings
 curl -o ~/.gnupg/gpg.conf https://raw.githubusercontent.com/drduh/config/master/gpg.conf
+
+# Permissions
+chmod 600 ~/.gnupg/gpg.conf
 
 # Import Key
 gpg --import $PUBLIC_KEY
