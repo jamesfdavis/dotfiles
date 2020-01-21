55792066597f92985ef9a5309690a39f82edecce
James F. Davis <ragingsmurf@gmail.com>
2020-01-21 13:16:30 -0500

    Begone 1passwd

--- brew-cask.sh
+++ brew-cask.sh
@@ -10,7 +10,7 @@ brew cask install virtualbox vagrant
 brew tap homebrew/cask-versions
 
 # daily
-brew cask install spectacle dropbox 1password flux evernote macdown brave-browser
+brew cask install spectacle dropbox flux evernote macdown brave-browser
 
 # developer tools 
 brew cask install iterm2 visual-studio-code slack
